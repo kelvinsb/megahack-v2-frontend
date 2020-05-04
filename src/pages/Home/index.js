0f00c42772c7e51635c5f120e73b4f7e30d3b5be
Nurian <nuriancoelho@hotmail.com>
2020-05-04 20:38:23 -0300

    frontend inicio

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,225 +1,122 @@
 import React from 'react';
-import MenuList from '@material-ui/core/MenuList';
-import MenuItem from '@material-ui/core/MenuItem';
-import { makeStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import DraftsIcon from '@material-ui/icons/Drafts';
-import SendIcon from '@material-ui/icons/Send';
-import PriorityHighIcon from '@material-ui/icons/PriorityHigh';
-
-import ButtonBase from '@material-ui/core/ButtonBase';
-import Button from '@material-ui/core/Button';
-
-
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-
 
 import 'typeface-roboto';
 
+
+import logo from '../../assets/1.png';
+import imageLogin from '../../assets/imageLogin.png';
+
 import './style.css';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    backgroundColor: '#f2f8f4',
-  },
-  root_menu: {
-    flexGrow: 1,
-  },
-  paper_background_menu: {
-    height: '100vh',
-    width: '270px',
-    backgroundColor: '#83DEA3',
-  },
-  menu_titulo: {
-    fontFamily: 'Source Sans Pro',
-    fontWeight: 'bold',
-    fontSize: '15px',
-    letterSpacing: '0.2em',
-    lineHeight: '21px',
-    textAlign: 'left',
-    color: '#fff',
-    paddingTop: '20px',
-
-  },
-  background_titulo: {
-    fontFamily: 'Source Sans Pro',
-    fontWeight: 'bold',
-    fontSize: '50px',
-    textAlign: 'left',
-    color: '#43425d',
-  },
-  h3: {
-    paddingLeft: '10px',
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: '#43425d',
-    width: '100vh',
-  },
-  container: {
-    width: '100vh',
-    height: '100px',
-    fontFamily: 'SourceSansPro',
-    fontSize: '50px',
-    fontWeight: 'bold',
-    fontStretch: 'normal',
-    fontStyle: 'normal',
-    lineHheight: 1.42,
-    letterSspacing: 'normal',
-    textAlign: 'left',
-    flexGrow: 1,
-    marginTop: '78px',
-    marginLeft: '69px',
-    backgroundColor: '#f2f8f4',
-  },
-  presentText: {
-    fontFamily: 'SourceSansPro',
-    fontSize: '35px',
-    fontWeight: 'normal',
-    fontStretch: 'normal',
-    fontStyle: 'normal',
-    lineHeight: 1.43,
-    letterSpacing: 'normal',
-    textAlign: 'left',
-  },
-  presentCard: {
-    margin: 10,
-  },
-  botaoRodape: {
-    display: 'flex',
-    justifyContent: 'space-around',
-  },
-  teste2: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    alignContent: 'flex-end',
-  },
-  footer: {
-    marginTop: 'auto',
-  },
-  button: {
-    backgroundColor: '#4ea16b',
-    color: '#ffffff',
-  },
-
-}));
-
 export default function Home() {
-  const classes = useStyles();
+
+
+  function handleLogar(e) {
+    console.log(e);
+  }
 
   return (
-    <Grid container xs={12} className={classes.root}>
+    <div>
+      <div className="menu">
+        <div className="buttonsMenu">
+          <button className="buttonMenu">
+            <p className="textButtonMenu">
+              Sobre
+          </p>
+          </button>
+          <button className="buttonMenu">
+            <p className="textButtonMenu">
+              Login
+          </p>
+          </button>
+          <button className="buttonMenu">
+            <p className="textButtonMenu">
+              Cadastre-se
+          </p>
+          </button>
+        </div>
+      </div>
+      <div className="boxLogo">
+        <img src={logo} className="logo" alt="Tarrafa" />
+      </div>
+      <div className="container">
 
-      <CssBaseline />
+        <div className="titleSobre">
+          <h1> Sobre </h1>
+        </div>
 
-      <Card className={classes.paper_background_menu} elevation={3}>
-        <Grid item xs={12} className={classes.menu_titulo}>
-          <h3 className={classes.h3}>Menu</h3>
-          <Paper className={classes.root_menu}>
-            <MenuList>
-              <MenuItem>
-                <ListItemIcon>
-                  <SendIcon fontSize="small" />
-                </ListItemIcon>
-                <Typography variant="inherit">Home</Typography>
-              </MenuItem>
-              <MenuItem>
-                <ListItemIcon>
-                  <PriorityHighIcon fontSize="small" />
-                </ListItemIcon>
-                <Typography variant="inherit">Meu Cartão</Typography>
-              </MenuItem>
-              <MenuItem>
-                <ListItemIcon>
-                  <DraftsIcon fontSize="small" />
-                </ListItemIcon>
-                <Typography variant="inherit" noWrap>Pessoas</Typography>
-              </MenuItem>
-            </MenuList>
-          </Paper>
-        </Grid>
+        <div className="descSobre">
+          <p> Tarrafa é uma ferramenta que colabora na organização de seu evento.
+          Pensando nas dores dos participantes, nós construímos um ambiente
+          simples e que oferece uma qualidade nos eventos que era perdida em
+          plataformas online. Tenha mais facilidade na navegação, na gerencia
+            de seus eventos, na realização de network e diversas outras vantagens. </p>
 
-        <CardActions>
-          <Button fullWidth className={classes.button}>Learn More</Button>
-        </CardActions>
+          <p> Conheça a plataforma e se encante com nova experiência em eventos
+            no mundo digital!  </p>
+        </div>
 
-        {/* <footer className={classes.footer}>
-          <Button variant="contained" fullWidth className={classes.button}>
-            Voltar para perfil
-          </Button>
-        </footer> */}
-      </Card>
-      <main className={classes.content}>
-        <Container className={classes.container}>
-          <Grid container spacing={3}>
-            <Grid item xs={12}>
-              <Paper>
-                Meu Cartão Virtual
-              </Paper>
-            </Grid>
-            <Grid className={classes.presentText}>
-              Esse é seu cartão de apresentação para os demais participantes do evento.
-              Preenche-o com suas preferências e, principalmente,
-              diga que tipo de network você procura.
-            </Grid>
-            <Grid className={classes.presentCard} item xs={12}>
-              <Paper>
-                <Grid container spacing={2}>
-                  <Grid item xs={12} sm container>
-                    <Grid item xs container direction="column" spacing={2}>
-                      <Grid item xs>
-                        <Grid item>
-                          <ButtonBase className={classes.image}>
-                            <img className={classes.img} alt="complex" src="/static/images/grid/complex.jpg" />
-                          </ButtonBase>
-                        </Grid>
-                        <Typography gutterBottom variant="subtitle1">
-                          NomeParticipante
-                        </Typography>
-                        <Typography gutterBottom variant="subtitle1">
-                          TipodeNetworkParticipante
-                        </Typography>
-                        <Typography gutterBottom variant="subtitle1">
-                          Ocupaçaoparticipante
-                        </Typography>
-                        <Typography variant="body1" gutterBottom>
-                          If the surface flow of the runoff is diverted and concentrated by drains that create constant
-                          flows over the beach above the sea or river level, the beach will be eroded. If the surface flow
-                          of the runoff is diverted and concentrated by drains that create constant flows over the beach
-                          above the sea or river level, the beach will be eroded. If the surface flow of the runoff is diverted
-                          and concentrated by drains that create constant flows over the beach above the sea or river level,
-                          the beach will be eroded. If the surface flow of the runoff is diverted and concentrated by drains.
-                          (DescriçãoDoParticipante)
-                        </Typography>
-                        <Typography gutterBottom variant="subtitle1">
-                          telefone
-                        </Typography>
-                        <Typography gutterBottom variant="subtitle1">
-                          Linkedin
-                        </Typography>
-                        <Typography gutterBottom variant="subtitle1">
-                          Email
-                        </Typography>
-                      </Grid>
-                    </Grid>
-                  </Grid>
-                </Grid>
-              </Paper>
-            </Grid>
-          </Grid>
-        </Container>
-      </main>
+        <div className="boxEntrar">
+          <div className="boxLogin">
+            <h1>
+              Entrar
+            </h1>
 
-    </Grid>
+            <div className="logar">
+              <div className="boxLogoLogin">
+                <img src={logo} className="logoLogin" alt="Tarrafa" />
+              </div>
+
+              <div className="formLogar">
+                <form onSubmit={handleLogar}>
+
+                  <input placeholder="E-mail do usuário" />
+                  <input type="password" placeholder="Senha" />
+
+                  <button className="botao" type="submit"> Entrar </button>
+
+                </form>
+              </div>
+            </div>
+
+            <div className="image">
+              <div className="boxImageLogin">
+                <img src={imageLogin} className="imageLogin" alt="Tarrafa" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="boxCadastrar">
+          <div className="boxCadastro">
+            <h1>
+              Realize seu cadastro
+            </h1>
+
+            <div className="cadastrar">
+              <div className="boxLogoLogin">
+                <img src={logo} className="logoLogin" alt="Tarrafa" />
+              </div>
+
+              <div className="formCadastrar">
+                <form onSubmit={handleLogar}>
+
+                  <input  type="text" className="input" placeholder="Nome completo" />
+                  <input  type="text" className="input" placeholder="E-mail" />
+                  <input  type="text" className="input" placeholder="Cidade" />
+                  <input  type="radio" className="inputRadio" value="Participante" /> Participante
+
+                  <button className="botao" type="submit"> Próximo </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
   );
 }
+
+
--- src/routes.js
+++ src/routes.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-// import Logon from './pages/Logon';
-import Register from './pages/Register';
-import Profile from './pages/Profile';
-import NewIncident from './pages/NewIncident';
-import MyCard from './pages/MyCard';
-
 import Home from './pages/Home';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        {/* <Route path="/" exact component={Logon} /> */}
-        <Route path="/register" component={Register} />
-        <Route path="/profile" component={Profile} />
-        <Route path="/incidents/new" component={NewIncident} />
-        <Route path="/MyCard" component={MyCard} />
-        <Route path="/" component={Home} />
+        <Route path="/Home" component={Home} />
       </Switch>
     </BrowserRouter>
   );
